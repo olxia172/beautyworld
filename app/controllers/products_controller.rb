90f000af6356b58919628200d10f9939783c3c09
olxia172 <kucharczyk.aleksandra@gmail.com>
2018-01-25 13:56:49 +0100

    Add basic function for product (create, show)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+  def index
+    @all_products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :brand, :capacity)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
 end
