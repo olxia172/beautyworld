d443ad94a921bec14e40c245aca96998e645f63e
olxia172 <kucharczyk.aleksandra@gmail.com>
2018-01-25 12:31:28 +0100

    Edit, update ingredient + checkboxes for ingredient functions in form

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,13 +1,13 @@
 class IngredientsController < ApplicationController
   before_action :load_current_function
-  before_action :find_ingredient, only: [:edit, :update, :destroy]
+  before_action :find_ingredient, only: [:show, :edit, :update, :destroy]
 
   def index
     @all_ingredients = Ingredient.count
-    # Rails.logger.info "Letter param to: #{letter_param.inspect}"
-    # Rails.logger.info "Function param to: #{function_param.inspect}"
-    if letter_param
-      @ingredients = Ingredient.where("name like ?", "#{letter_param}%")
+    # Rails.logger.info "Letter param to: #{letter_sort_param.inspect}"
+    # Rails.logger.info "Function param to: #{function_sort_param.inspect}"
+    if letter_sort_param
+      @ingredients = Ingredient.where("name like ?", "#{letter_sort_param}%")
     elsif @function
       @ingredients = @function.ingredients
     else
@@ -16,7 +16,18 @@ class IngredientsController < ApplicationController
     @functions = IngredientFunction.all.order(name: :asc)
   end
 
+  def show
+  end
+
   def edit
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'edit'
+    end
   end
 
 private
@@ -25,15 +36,19 @@ private
     @ingredient = Ingredient.find(params[:id])
   end
 
-  def letter_param
+  def ingredient_params
+    params.require(:ingredient).permit(:name, ingredient_function_ids: [])
+  end
+
+  def letter_sort_param
     params[:sign] if params[:sign] && params[:sign].length == 1
   end
 
-  def function_param
+  def function_sort_param
     params[:function]
   end
 
   def load_current_function
-    @function = IngredientFunction.find(function_param) if function_param
+    @function = IngredientFunction.find(function_sort_param) if function_sort_param
   end
 end
--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -1,2 +1,8 @@
 module IngredientsHelper
+  def ingredient_functions(ingredient)
+    ingredient_functions = ingredient.ingredient_function_ids.map do |id|
+      IngredientFunction.find(id).name
+    end
+    ingredient_functions.join(', ')
+  end
 end
