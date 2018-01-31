f1ab9f32e23ea27619ec4a1e619fc95d7dcd5a63
olxia172 <kucharczyk.aleksandra@gmail.com>
2018-01-31 13:11:17 +0100

    Add image to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   validates :name, presence: true, uniqueness: true
   validates :capacity, presence: true
 
   has_and_belongs_to_many :ingredients
   belongs_to :brand
   belongs_to :subcategory
+
 
   attr_reader :ingredient_tokens
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130182700) do
+ActiveRecord::Schema.define(version: 20180131114047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20180130182700) do
     t.datetime "updated_at", null: false
     t.bigint "brand_id"
     t.bigint "subcategory_id"
+    t.string "image"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["subcategory_id"], name: "index_products_on_subcategory_id"
   end
