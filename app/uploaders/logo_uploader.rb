c36a9d1295c418caa4776afc6ea4fda7dea167f3
olxia172 <kucharczyk.aleksandra@gmail.com>
2018-02-19 12:42:28 +0100

    Correct logo file name during brand update

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -41,7 +41,13 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    model_name + ".#{file.extension}" if original_filename
+  end
+
+  private
+
+  def model_name
+    model.name.parameterize.underscore
+  end
 end
