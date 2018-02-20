87f9da4c66e344f5d7c06e328eff7a8d99978fb8
olxia172 <kucharczyk.aleksandra@gmail.com>
2018-02-20 12:41:44 +0100

    Change product views

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,4 +1,11 @@
 class MainPageController < ApplicationController
   def index
+    @products = last_added_products
+  end
+
+  private
+
+  def last_added_products
+    Product.order(created_at: :desc).limit(6)
   end
 end
